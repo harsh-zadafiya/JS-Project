0af125a7cc26b820af17b05d934f5349734554b1
Harsh Zadafiya <harshzadafiya@Harshs-MacBook-Air.local>
2023-03-31 18:06:01 -0400

    remove console and add comment

--- To-Do/to-do.js
+++ To-Do/to-do.js
@@ -107,7 +107,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // set initial clock display and then set interval timer to display
   // new time every second. Don't store timer object because it
   // won't be needed - clock will just run.
-  console.log("called");
   getTaskFromLS();
   window.localStorage.removeItem("tasks");
 });
